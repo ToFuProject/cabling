c295a226e52c561c4644ebf2e09eb24be7e7f1f9
Didier Vezinet <dvezinet@cfs.energy>
2024-08-01 15:23:04 -0400

    [#main] add_known() also adds connector_type now

---
+++ cabling/_known_connectors.py
@@ -0,0 +1,114 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  1 13:24:57 2024
+
+@author: dvezinet
+"""
+
+from . import _known_connections
+
+#############################################
+#############################################
+#    Connection types
+#############################################
+
+
+def get():
+
+    dout = {}
+
+    # -------------
+    # coax BNC cables
+    # -------------
+
+    for imp in ['50Hz', '75Hz']:
+        for mf in [('M', 'F'), ('M', 'M'), ('F', 'F')]:
+            key = f"coax_BNC_{imp}_{''.join(mf)}"
+            dout[key] = {
+                'description': f'coaxial cable with {mf} connections',
+                'connections': {
+                    'ptA': {'type': f"BNC_{imp}_{mf[0]}"},
+                    'ptB': {'type': f"BNC_{imp}_{mf[1]}"},
+                },
+            }
+
+    # -------------
+    # coax SHV cables
+    # -------------
+
+    for mf in [('M', 'F'), ('M', 'M'), ('F', 'F')]:
+        key = f"coax_SHV_{imp}_{''.join(mf)}"
+        dout[key] = {
+            'description': f'coaxial cable with {mf} connections',
+            'connections': {
+                'ptA': {'type': f"SHV_{mf[0]}"},
+                'ptB': {'type': f"SHV_{mf[1]}"},
+            },
+        }
+
+    # -------------
+    # MI cables
+    # -------------
+
+    dout['MI'] = {
+        'description': 'Mineral insulated cable',
+        'connections': {
+            'ptA': {'type': "MI_Term"},
+            'ptB': {'type': "MI_Term"},
+        },
+    }
+
+    # -------------
+    # Lemo cables
+    # -------------
+
+
+    # DECTRIS external trigger
+    dout['coax_Lemo'] = {
+        'description': 'coaxial cable with Lemo® Type 00 (NIM/CAMAC)',
+        'connections': {
+            'ptA': {'type': "Lemo® Type 00"},
+            'ptB': {'type': "Lemo® Type 00"},
+        },
+    }
+
+    # -------------
+    # power cables
+    # -------------
+
+    # DECTRIS power
+    dout['DECTRIS_pow'] = {
+        'description': 'power cable for DECTRIS EIGER 2 S 500K',
+        'connections': {
+            'ptA': {'type': "DECTRIS_pow"},
+            'ptB': {'type': "DECTRIS_pow"},
+        },
+    }
+
+    # -------------
+    # poptics fiber
+    # -------------
+
+    # SFP+ optics fiber
+    dout['10Gb_SM']= {
+        'description': '10 GbE-LR single mode optic fiber for DECTRIS EIGER 2',
+        'connections': {
+            'ptA': {'type': "SFP+"},
+            'ptB': {'type': "SFP+"},
+        },
+    }
+
+    # -------------
+    # tubes
+    # -------------
+
+    # SFP+ optics fiber
+    dout['DECTRIS_cool'] = {
+        'description': 'cooling pipes connections for DECTRIS cameras',
+        'connections': {
+            'ptA': {'type': "DECTRIS_cool"},
+            'ptB': {'type': "DECTRIS_cool"},
+        },
+    }
+
+    return dout
