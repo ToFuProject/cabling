9ece92289ca1685c9f975ef7b4cffbd3400b4601
Didier Vezinet <dvezinet@cfs.energy>
2024-08-02 12:03:11 -0400

    [#main] cleaning up inputs_devices

---
+++ cabling/inputs/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  1 09:27:03 2024
+
+@author: dvezinet
+"""
+
+
+from . import _connection_types
+from . import _connector_types
+from . import _connector_models
+from . import _connectors
+from . import _device_types
+from . import _device_model
+from . import _devices
