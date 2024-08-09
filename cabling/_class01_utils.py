c02ee87dbe54cd9a3795a1e753b7d2c393b2fcc7
Didier Vezinet <dvezinet@cfs.energy>
2024-08-09 08:14:00 -0400

    [#main] updated .gitinore and added _config.py

---
+++ cabling/_class01_utils.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  8 18:01:13 2024
+
+@author: dvezinet
+"""
---
+++ cabling/_config.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug  9 08:00:57 2024
+
+@author: dvezinet
+"""
+
+
+_SYSTEMS = [f'L{ii}' for ii in range(1, 6)]
+_SYSTEMS_KEY = [f'L{ii}' for ii in range(2, 6)]
